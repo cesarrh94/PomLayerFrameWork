7291ca2c9bd0ff702523b93b58496acf138866ae
cirh-unosquare <94cesarrh@gmail.com>
2022-12-13 18:16:07 -0600

    refactoring testcase to remove sleep() functions

--- src/test/java/testcases/AddToCartTest.java
+++ src/test/java/testcases/AddToCartTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.SearchResultsPage;
+import pages.SignInPage;
 import utils.BaseDriver;
 import utils.JsonFileReader;
 
@@ -13,17 +16,32 @@ public class AddToCartTest {
     BaseDriver baseDriver;
     JsonFileReader reader = new JsonFileReader();
 
+    // pageObjects
+    HomePage homePage;
+    SignInPage signInPage;
+    SearchResultsPage searchResultsPage;
+
     @BeforeMethod
     public void setUp() {
         baseDriver = new BaseDriver(driver);
         driver = baseDriver.setUpDriver();
         baseDriver.navigateTo(reader.getAttribute("url"));
+        homePage = new HomePage(driver);
+        signInPage = new SignInPage(driver);
+        searchResultsPage = new SearchResultsPage(driver);
     }
 
 
     @Test
     public void testAddProductToCart() {
-        System.out.println("Hello from test");
+        homePage.goToLogin();
+        String email = reader.getAttribute("email");
+        String password = reader.getAttribute("password");
+        signInPage.signIn(email, password);
+        String productName = "Samsung Galaxy S9 64GB";
+        homePage.searchProduct(productName);
+        searchResultsPage.storePriceProduct();
+        searchResultsPage.selectFirstResult();
     }
 
 
