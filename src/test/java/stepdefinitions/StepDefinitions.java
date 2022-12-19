ea38c118a16529d077d08d93904c1b9048dc567e
cesar rodriguez <94cesarrh@gmail.com>
2022-12-18 23:54:48 -0600

    creation of step definition methods

---
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -0,0 +1,93 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("I am on signin page")
+    public void i_am_on_signin_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I enter username and password")
+    public void i_enter_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I click sign in button")
+    public void i_click_sign_in_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I search for a {string}")
+    public void i_search_for_a(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I select the first product on the results list and save the price")
+    public void i_select_the_first_product_on_the_results_list_and_save_the_price() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Validate the price from the product page agaisnt search page")
+    public void validate_the_price_from_the_product_page_against_search_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Validate the price from the product page against search page")
+    public void validate_the_price_from_the_product_page_against_search_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("I am on the cart page")
+    public void i_am_on_the_cart_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Validate the price from the cart page against search page")
+    public void validate_the_price_from_the_cart_page_against_search_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Validate the cart has {int} item on it")
+    public void validate_the_cart_has_item_on_it(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I search for a {string}")
+    public void i_search_for_a(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("I select the first product on the result list")
+    public void i_select_the_first_product_on_the_result_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("I click on add to cart button")
+    public void i_click_on_add_to_cart_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Validate the cart has {int} items on it")
+    public void validate_the_cart_has_items_on_it(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
